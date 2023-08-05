3b3cbee9203da524f616ceb67dbc938bfbf86aee
Skynet <robo@taktile.com>
2023-08-05 17:16:47 +0200

    Rearrange

--- src/__tests__/minio.test.ts
+++ src/__tests__/minio.test.ts
@@ -44,38 +44,6 @@ describe("mps3", () => {
 
   
 
-  test("Parallel puts commute (cold manifest)", async () => {
-    const manifests = [
-      {
-        key: Math.random().toString(),
-      },
-    ];
-    const n = 3;
-    const clients = [...Array(n)].map((_) => getClient());
-    const rand_keys = [...Array(n)].map(
-      (_, i) => `parallel_put/${i}_${Math.random().toString()}`
-    );
-
-    // put in parallel
-    await Promise.all(
-      rand_keys.map((key, i) =>
-        clients[i].put(key, i, {
-          manifests,
-        })
-      )
-    );
-
-    // read in parallel
-    const reads = await Promise.all(
-      rand_keys.map((key, i) =>
-        clients[n - i - 1].get(key, {
-          manifest: manifests[0],
-        })
-      )
-    );
-
-    expect(reads).toEqual([...Array(n)].map((_, i) => i));
-  });
 
   test("Can see other's mutations after populating cache", async () => {
     const mps3 = getClient();
@@ -214,6 +182,39 @@ describe("mps3", () => {
     // read in parallel
     const reads = await Promise.all(
       rand_keys.map((key, i) => clients[n - i - 1].get(key))
+    );
+
+    expect(reads).toEqual([...Array(n)].map((_, i) => i));
+  });
+
+  test("Parallel puts commute (cold manifest)", async () => {
+    const manifests = [
+      {
+        key: Math.random().toString(),
+      },
+    ];
+    const n = 3;
+    const clients = [...Array(n)].map((_) => getClient());
+    const rand_keys = [...Array(n)].map(
+      (_, i) => `parallel_put/${i}_${Math.random().toString()}`
+    );
+
+    // put in parallel
+    await Promise.all(
+      rand_keys.map((key, i) =>
+        clients[i].put(key, i, {
+          manifests,
+        })
+      )
+    );
+
+    // read in parallel
+    const reads = await Promise.all(
+      rand_keys.map((key, i) =>
+        clients[n - i - 1].get(key, {
+          manifest: manifests[0],
+        })
+      )
     );
 
     expect(reads).toEqual([...Array(n)].map((_, i) => i));
