e2585344dfaeb741f380fb550ad3b585962812b5
Skynet <robo@taktile.com>
2023-09-30 15:17:09 +0200

    Can save to disk and restore labels

--- src/operationQueue.ts
+++ src/operationQueue.ts
@@ -1,13 +1,14 @@
 import { OMap } from "OMap";
-import { JSONValue, DeleteValue } from "./types";
+import { JSONValue, DeleteValue, uuid } from "./types";
 import { UseStore, get, set, del, keys } from "idb-keyval";
 
 export type Operation = Promise<unknown>;
 
 export class OperationQueue {
+  private session = uuid();
   proposedOperations: Map<Operation, Map<URL, JSONValue | DeleteValue>> =
     new Map();
-  operationLabels: Map<string, [Operation, string?]> = new Map(); // Second value is the IndexedDB key if used
+  operationLabels: Map<string, Operation> = new Map();
   private db?: UseStore;
   private lastIndex: number = 0;
 
@@ -16,45 +17,57 @@ export class OperationQueue {
   }
 
   async propose(write: Operation, values: Map<URL, JSONValue | DeleteValue>) {
-    this.proposedOperations.set(write, values);
     if (this.db) {
       this.lastIndex++;
       const key = `entry-${this.lastIndex}`;
+      (<any>write)[this.session] = this.lastIndex;
       await set(
         key,
         [...values.entries()].map(([url, val]) => [url.toString(), val]),
         this.db
       );
     }
+
+    this.proposedOperations.set(write, values);
   }
 
-  async label(write: Operation, version: string) {
-    const key = this.db ? `entry-${this.lastIndex}` : undefined;
-    this.operationLabels.set(version, [write, key]);
+  async label(write: Operation, label: string) {
+    this.operationLabels.set(label, write);
+
+    if (this.db) {
+      const index = (<any>write)[this.session];
+
+      if (index === undefined)
+        throw new Error("Cannot label an unproposed operation");
+      await set(`label-${index}`, label, this.db);
+    }
   }
 
   async confirm(label: string) {
     if (this.operationLabels.has(label)) {
-      const [operation, key] = this.operationLabels.get(label)!;
+      const operation = this.operationLabels.get(label)!;
       this.proposedOperations.delete(operation);
       this.operationLabels.delete(label);
-      if (key && this.db) {
-        await del(key, this.db);
+      if (this.db) {
+        const index = (<any>operation)[this.session];
+        await del(`entry-${index}`, this.db);
+        await del(`label-${index}`, this.db);
       }
     }
   }
 
-  async cancel(write: Operation) {
+  async cancel(operation: Operation) {
     let keyToDelete: string | undefined;
     this.operationLabels.forEach((value, key) => {
-      if (value[0] === write) {
-        keyToDelete = value[1];
+      if (value === operation) {
         this.operationLabels.delete(key);
       }
     });
-    this.proposedOperations.delete(write);
-    if (keyToDelete && this.db) {
-      await del(keyToDelete, this.db);
+    this.proposedOperations.delete(operation);
+    if (this.db) {
+      const index = (<any>operation)[this.session];
+      await del(`entry-${index}`, this.db);
+      await del(`label-${index}`, this.db);
     }
   }
 
@@ -70,9 +83,12 @@ export class OperationQueue {
 
   async restore(
     store: UseStore,
-    schedule: (write: Map<URL, JSONValue | DeleteValue>) => Promise<unknown>
+    schedule: (write: Map<URL, JSONValue | DeleteValue>) => Operation
   ) {
     this.db = store;
+    this.proposedOperations.clear();
+    this.operationLabels.clear();
+    this.lastIndex = 0;
 
     const allKeys: string[] = await keys(this.db);
     const entries = allKeys
@@ -80,17 +96,28 @@ export class OperationQueue {
       .sort();
 
     for (const key of entries) {
-      const entriesRaw = await get<[string, JSONValue | DeleteValue][]>(
+      const index = parseInt(key.split("-")[1]);
+      const entry = await get<[string, JSONValue | DeleteValue][]>(
         key,
         this.db
       );
-      const entries: [URL, JSONValue | DeleteValue][] = entriesRaw!.map(
+      const label = await get<string>(`label-${index}`, this.db);
+      if (!entry) continue;
+
+      const entries: [URL, JSONValue | DeleteValue][] = entry.map(
         ([url, val]) => [new URL(url), val]
       );
       const values = new Map<URL, JSONValue | DeleteValue>(entries);
       const op = schedule(values);
-      this.propose(op, values);
-      this.lastIndex = Math.max(this.lastIndex, parseInt(key.split("-")[1]));
+      (<any>op)[this.session] = index;
+      this.proposedOperations.set(op, values);
+
+      if (label) {
+        console.log("about to label");
+        this.label(op, label);
+      }
+
+      this.lastIndex = Math.max(this.lastIndex, index);
     }
   }
 }
