24f1777f2e7ca09ea62dcb0785051f4d5306a0a6
Skynet <robo@taktile.com>
2023-10-01 20:31:04 +0200

    Add tolerance to network outages

--- src/S3ClientLite.ts
+++ src/S3ClientLite.ts
@@ -15,25 +15,51 @@ export type FetchFn = (
   options?: object | undefined,
 ) => Promise<Response>;
 
+
+const retry = async <T>(
+  fn: () => Promise<T>,
+  options: {
+    retries: number;
+    delay: number;
+  } = {
+    retries: Number.MAX_VALUE,
+    delay: 100,
+  }
+): Promise<T> => {
+  try {
+    return await fn();
+  } catch (e) {
+    if (options.retries > 0) {
+      await new Promise((resolve) => setTimeout(resolve, options.delay));
+      return await retry(fn, {
+        retries: options.retries - 1,
+        delay: options.delay,
+      });
+    } else {
+      throw e;
+    }
+  }
+};
+
 export class S3ClientLite {
-  client: FetchFn;
+  fetch: FetchFn;
   endpoint: string;
   parser: DOMParser;
   constructor(fetch: FetchFn, endpoint: string, parser: DOMParser) {
-    this.client = fetch;
+    this.fetch = fetch;
     this.endpoint = endpoint;
     this.parser = parser;
   }
 
   async listObjectV2(
-    command: ListObjectsV2CommandInput,
+    command: ListObjectsV2CommandInput
   ): Promise<ListObjectsV2CommandOutput> {
     // retry loop for 429
     for (let i = 0; i < 10; i++) {
       const url = `${this.endpoint}/${command.Bucket!}/?list-type=2&prefix=${
         command.Prefix
       }`;
-      const response = await this.client(url, {});
+      const response = await retry(() => this.fetch(url, {}));
       if (response.status === 200) {
         const xml = await response.text();
         const result = parseListObjectsV2CommandOutput(xml, this.parser);
@@ -43,7 +69,7 @@ export class S3ClientLite {
         await new Promise((resolve) => setTimeout(resolve, 1000));
       } else {
         throw new Error(
-          `Unexpected response: ${response.status} ${response.text()}`,
+          `Unexpected response: ${response.status} ${response.text()}`
         );
       }
     }
@@ -51,19 +77,21 @@ export class S3ClientLite {
   }
 
   async putObject(
-    command: PutObjectCommandInput,
+    command: PutObjectCommandInput
   ): Promise<PutObjectCommandOutput> {
     const url = `${this.endpoint}/${command.Bucket!}/${command.Key}`;
-    const response = await this.client(url, {
-      method: "PUT",
-      body: <string>command.Body,
-      headers: {
-        "Content-Type": "application/json",
-        ...(command.ChecksumSHA256 && {
-          "x-amz-content-sha256": command.ChecksumSHA256,
-        }),
-      },
-    });
+    const response = await retry(() =>
+      this.fetch(url, {
+        method: "PUT",
+        body: <string>command.Body,
+        headers: {
+          "Content-Type": "application/json",
+          ...(command.ChecksumSHA256 && {
+            "x-amz-content-sha256": command.ChecksumSHA256,
+          }),
+        },
+      })
+    );
     if (response.status != 200)
       throw new Error(`Failed to PUT: ${await response.text()}`);
     return {
@@ -78,12 +106,14 @@ export class S3ClientLite {
   }
 
   async deleteObject(
-    command: DeleteObjectCommandInput,
+    command: DeleteObjectCommandInput
   ): Promise<DeleteObjectCommandOutput> {
     const url = `${this.endpoint}/${command.Bucket!}/${command.Key}`;
-    const response = await this.client(url, {
-      method: "DELETE",
-    });
+    const response = await retry(() =>
+      this.fetch(url, {
+        method: "DELETE",
+      })
+    );
     return {
       $metadata: {
         httpStatusCode: response.status,
@@ -92,17 +122,19 @@ export class S3ClientLite {
   }
 
   async getObject(
-    command: GetObjectCommandInput,
+    command: GetObjectCommandInput
   ): Promise<GetObjectCommandOutput> {
     const url = `${this.endpoint}/${command.Bucket!}/${command.Key}?${
       command.VersionId ? `versionId=${command.VersionId}` : ""
     }`;
-    const response = await this.client(url, {
-      method: "GET",
-      headers: {
-        "If-None-Match": command.IfNoneMatch!,
-      },
-    });
+    const response = await retry(() =>
+      this.fetch(url, {
+        method: "GET",
+        headers: {
+          "If-None-Match": command.IfNoneMatch!,
+        },
+      })
+    );
     if (response.status == 304) {
       const err = new Error();
       err.name = "304";
