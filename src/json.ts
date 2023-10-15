a550e853c92a693c1fa4a9a8bd7da98930609184
tom.larkworthy@taktile.com <robo@taktile.com>
2023-10-15 22:24:55 +0200

    Fix merge

--- src/json.ts
+++ src/json.ts
@@ -18,21 +18,23 @@ export const clone = (state: JSONValue) => JSON.parse(JSON.stringify(state));
  * JSON Merge Patch (RFC 7386)
  * Update target JSON with a merge patch
  */
-export function merge<T>(target: T, patch: Partial<T>): T {
+export function merge<T>(target: T, patch: JSONValue): T {
   // If patch is an array or a primitive, just return it
   if (Array.isArray(patch) || typeof patch !== "object" || patch === null) {
     return <T>patch;
   }
 
   // Ensure target is an object
+  const target_json: any =
+    typeof target !== "object" || target === null ? {} : target;
 
   for (let key in patch) {
     if (patch[key] === null) {
-      delete target[key];
+      delete target_json[key];
     } else {
-      target[key] = merge(target[key], patch[key]!);
+      target_json[key] = merge(target_json[key], patch[key]);
     }
   }
 
-  return target;
+  return <T>target_json;
 }
